a313f6dfa050f1d4f435e88dc47cd69d50a5d351
bhvy4 <khanna.micky01@gmail.com>
2023-01-15 19:39:11 +0530

    Initial commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+import "./Home.css";
+
+export default function Home() {
+  const {
+    data,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3000/recipes");
+  return (
+    <div className="home">
+      {isPending && <p className="loading"> loading... </p>}
+      {error && <p className="error">{error}</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
+}
