e17a047d8fa59de9850560a1dbf10ac9af651c30
bhvy4 <khanna.micky01@gmail.com>
2023-01-16 21:50:45 +0530

    changes in Recipe component

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,8 +1,25 @@
-import React from 'react'
-import './Recipe.css'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
+import "./Recipe.css";
 
 export default function Recipe() {
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { data : recipe, isPending, error } = useFetch(url);
   return (
-    <div>Recipe</div>
-  )
+    <div className="recipe">
+      {isPending && <p className="loading">loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {recipe && (
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>{recipe.cookingTime} to make.</p>
+          <ul>{recipe.ingredients.map(ing => <li key={ing}>{ing}</li> )}</ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
+    </div>
+  );
 }
